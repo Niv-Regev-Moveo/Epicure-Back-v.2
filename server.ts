ea3d9c0c4e0cfcad8b9e31a38940d1e66e98b125
niv-regev-moveo <nivr@moveo.co.il>
2024-06-23 10:52:13 +0300

    test

--- server.ts
+++ server.ts
@@ -1,11 +1,8 @@
-const express =  require('express')
-const app = express()
-const mongoose = require('mongoose')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 
-
-
-app.get("/",(req,res)=>{
-    res.send("Connected with Express , Rest API app")
-})
-app.listen (3000,() => console.log('Server Started'));
-
+app.get("/", (req, res) => {
+  res.send("Connected with Express , Rest API ");
+});
+app.listen(3000, () => console.log("Server Started"));
